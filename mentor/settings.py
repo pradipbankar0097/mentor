5aa0e719a1610fa28f9eb63fac2f98adfa3dd78c
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-07-24 18:15:03 +0530

    static added

--- mentor/settings.py
+++ mentor/settings.py
@@ -125,11 +125,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # manually added
-
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    
+    os.path.join(BASE_DIR , "static"),
 ]
-
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
