5e2e9fb529a136e1976f741cd346487b4fa1e01e
pradipbankar0097 <pradipbankar0097@gmail.com>
2021-08-17 21:57:32 +0530

    gitignore modified

---
+++ mentorhome/fireconfig.py
@@ -0,0 +1,9 @@
+config = {
+    "apiKey": "AIzaSyDHXFxTpdFw9Qi-kOjyPlQtD9fxZyOBvp0",
+    "authDomain": "mentor-64363.firebaseapp.com",
+    "databaseURL": "mentor-64363-default-rtdb.firebaseio.com/",
+    "projectId": "mentor-64363",
+    "storageBucket": "mentor-64363.appspot.com",
+    "messagingSenderId": "197471931792",
+    "appId": "1:197471931792:web:bab14d60ec5b5234553715"
+}
